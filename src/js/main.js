6d88740d8fa0abd0d675e5048ff38fedb3e250f3
QadamosssQ <95488005+QadamosssQ@users.noreply.github.com>
2022-09-22 20:57:39 +0200

    add ui on clicker and all styles

--- src/js/main.js
+++ src/js/main.js
@@ -14,3 +14,33 @@ function counting() {
     let count = document.getElementById("conter").innerHTML = "Punkty: "+ counter;
 }
 
+
+function times() {
+
+
+    let local_date = new Date();
+
+    let time_hours = local_date.getHours();
+    let time_minutes = local_date.getMinutes();
+    let time_seconds = local_date.getSeconds();
+
+
+    if(time_hours ===21 && time_minutes === 37 && time_seconds === 0){
+        alert("papież!")
+    }
+
+
+    let time = document.getElementById("timer").innerHTML = "Czas: " + time_hours + ":" + time_minutes + ":" + time_seconds;
+}
+
+
+function show_time(){
+    setInterval(times, 1000);
+
+}
+
+
+
+// show_time()
+
+
